1a9eea3cfc23b64399e6d3a37a15e2aa2d8ed7f8
aashwinshukla <aashwinshukla585@gmail.com>
2026-07-15 19:03:08 +0530

    Add functions to display available cities and buses in the bus service application

--- code/bus.c
+++ code/bus.c
@@ -12,6 +12,8 @@ bool flag=true;
 
 void userData();
 int yourChoice();
+void cities();
+void buses();
 
 int main(){
 
@@ -53,11 +55,11 @@ while(flag==true){
             break;
             
             case 2:
-            printf("buses info coming soon");
+            buses();
             break;
 
             case 3:
-            printf("cities coming soon");
+            cities();
             break;
 
             case 4:
@@ -98,4 +100,43 @@ int yourChoice(){
     scanf("%d", &local_choice);
 
     return local_choice;
+}
+
+void cities(){
+    printf("\n========= AVAILABLE CITIES & ROUTES =========\n");
+    printf("1. Mumbai         [West Hub - Commercial Capital]\n");
+    printf("2. Pune           [West Hub - IT & Education]\n");
+    printf("3. Ahmedabad      [West Hub - Manufacturing]\n");
+    printf("4. Goa            [West Hub - Coastal Tourism]\n");
+    printf("--------------------------------------------------\n");
+    printf("5. Delhi          [North Hub - National Capital]\n");
+    printf("6. Jaipur         [North Hub - Heritage Tourism]\n");
+    printf("7. Chandigarh     [North Hub - Planned City]\n");
+    printf("--------------------------------------------------\n");
+    printf("8. Bengaluru      [South Hub - Silicon Valley]\n");
+    printf("9. Hyderabad      [South Hub - Cyberabad Hub]\n");
+    printf("10. Chennai       [South Hub - Automobile Capital]\n");
+    printf("--------------------------------------------------\n");
+    printf("11. Kolkata       [East Hub - Cultural Capital]\n");
+    printf("==================================================\n");
+    printf("Note: Inter-city buses run every 30 minutes.\n");
+}
+
+void buses(){
+    printf("\n========= AVAILABLE BUSES & IDENTIFICATION =========\n");
+    printf("1.  [MH-12-AS-4521]  Bharat Express (AC Sleeper)\n");
+    printf("2.  [MH-14-LX-8890]  Deccan Queen Transports (Luxury Seater)\n");
+    printf("3.  [GJ-01-EE-3214]  Western Fleet (Electric Express)\n");
+    printf("4.  [GA-03-TR-7765]  Coastal Cruise Liners (Multi-Axle AC)\n");
+    printf("-----------------------------------------------------------\n");
+    printf("5.  [DL-01-NE-1092]  Capital Flyer (Premium Sleeper)\n");
+    printf("6.  [RJ-14-HT-5543]  Pink City Travels (Non-AC Seater)\n");
+    printf("7.  [CH-02-EX-9011]  Northern Shuttle (AC Express)\n");
+    printf("-----------------------------------------------------------\n");
+    printf("8.  [KA-03-SV-2287]  Silicon Valley Voyager (Multi-Axle AC)\n");
+    printf("9.  [TS-09-CB-6641]  Cyberabad Cruiser (AC Sleeper)\n");
+    printf("10. [TN-01-AC-4432]  Coromandel Connect (Electric Express)\n");
+    printf("-----------------------------------------------------------\n");
+    printf("11. [WB-02-CC-1254]  Eastern Star Liners (Luxury Seater)\n");
+    printf("===========================================================\n");
 }
