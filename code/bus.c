845006835ccaa6539bf7aad8fff6362e138d51ef
aashwinshukla <aashwinshukla585@gmail.com>
2026-07-15 18:54:53 +0530

    Add user interaction for bus service options and refactor userData function

--- code/bus.c
+++ code/bus.c
@@ -1,13 +1,17 @@
 #include<stdio.h>
+#include<stdbool.h>
+
 char name[25];
 char surname[25];
 int year, month, day;
 int code;
 long long contact;
 char email[40];
+int choice;
+bool flag=true;
 
 void userData();
-
+int yourChoice();
 
 int main(){
 
@@ -30,14 +34,50 @@ int main(){
     printf("\nEnter you Email:");
     scanf(" %s", email);
 
-    // userData( year, month, day, code, contact, 
-    //     name, email);
+    printf("\n\n---Thanks for Login---\n\n");
+    yourChoice(choice);
+    
+    // printf("whats on your mind?");
+    // printf("\n1. Bus Reservation ");
+    // printf("\n2. Buses");
+    // printf("\n3. Cities");
+    // printf("\n4. Profile");
+    // printf("\n5. Exit\n");
 
+    // scanf("%d", &choice);
+while(flag==true){
+    choice = yourChoice(choice);
+        switch(choice){
+            case 1:
+            printf("ticket reservetion coming soon ");
+            break;
+            
+            case 2:
+            printf("buses info coming soon");
+            break;
 
+            case 3:
+            printf("cities coming soon");
+            break;
+
+            case 4:
+            userData();
+            break;
+
+            case 5:
+            printf("thanks for using our service");
+            flag = false;
+            break;
+
+            default:
+            printf("wrong input ,try again!");
+            break;
+        }
+
+    }
 }
 
-void userData(int year, int month, int day, int code, long long contact,
-                char name[], char email[]){
+void userData(){
                     printf("\n----YOUR DATA----\n");
                     printf("Name: %s %s\n", name, surname);
                     printf("Date of Birth :%d/%d/%d\n", year, month, day);
@@ -45,3 +85,17 @@ void userData(int year, int month, int day, int code, long long contact,
                     printf("mobile number: +%d-%lld", code, contact);
                     printf("\nEmail: %s", email);
                 }
+
+int yourChoice(){
+    int local_choice;
+    printf("whats on your mind?");
+    printf("\n1. Bus Reservation ");
+    printf("\n2. Buses");
+    printf("\n3. Cities");
+    printf("\n4. Profile");
+    printf("\n5. Exit\n");
+
+    scanf("%d", &local_choice);
+
+    return local_choice;
+}
